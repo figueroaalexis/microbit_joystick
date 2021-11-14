e5ddc44f8532a7155b08130a3695ed3532e70430
figueroaalexis <16774417+figueroaalexis@users.noreply.github.com>
2021-11-14 13:13:49 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+WSJoyStick.onKey(KEY.F, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("F")
+})
+WSJoyStick.onKey(KEY.A, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("A")
+})
+WSJoyStick.onKey(KEY.E, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("E")
+})
+WSJoyStick.onKey(KEY.D, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("D")
+})
+WSJoyStick.onKey(KEY.B, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("B")
+})
+WSJoyStick.onKey(KEY.C, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    basic.showString("C")
+})
+WSJoyStick.JoyStickInit()
+basic.forever(function () {
+	
+})
