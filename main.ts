7689af53867ccd700d3f277fbd7463e1424003ef
figueroaalexis <16774417+figueroaalexis@users.noreply.github.com>
2021-11-14 16:34:22 -0400

    Completed code for indicating X and Y axes.

--- main.ts
+++ main.ts
@@ -1,28 +1,119 @@
 WSJoyStick.onKey(KEY.F, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("F")
     basic.showString("F")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
 })
 WSJoyStick.onKey(KEY.A, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("A")
     basic.showString("A")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
 })
 WSJoyStick.onKey(KEY.E, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("E")
     basic.showString("E")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
 })
 WSJoyStick.onKey(KEY.D, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("D")
     basic.showString("D")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
 })
 WSJoyStick.onKey(KEY.B, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("B")
     basic.showString("B")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+        basic.showIcon(IconNames.Heart)
+    }
 })
 WSJoyStick.onKey(KEY.C, function () {
-    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
+    radio.sendString("C")
     basic.showString("C")
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Sixteenth))
 })
 WSJoyStick.JoyStickInit()
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogPin.P1) > 510 && pins.analogReadPin(AnalogPin.P1) < 530 && (pins.analogReadPin(AnalogPin.P2) > 510 && pins.analogReadPin(AnalogPin.P2) < 530)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 115 && pins.analogReadPin(AnalogPin.P1) < 510 && (pins.analogReadPin(AnalogPin.P2) > 510 && pins.analogReadPin(AnalogPin.P2) < 530)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 0 && pins.analogReadPin(AnalogPin.P1) < 115 && (pins.analogReadPin(AnalogPin.P2) > 510 && pins.analogReadPin(AnalogPin.P2) < 530)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 530 && pins.analogReadPin(AnalogPin.P1) < 760 && (pins.analogReadPin(AnalogPin.P2) > 510 && pins.analogReadPin(AnalogPin.P2) < 530)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 760 && pins.analogReadPin(AnalogPin.P1) < 1024 && (pins.analogReadPin(AnalogPin.P2) > 510 && pins.analogReadPin(AnalogPin.P2) < 530)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 510 && pins.analogReadPin(AnalogPin.P1) < 530 && (pins.analogReadPin(AnalogPin.P2) > 115 && pins.analogReadPin(AnalogPin.P2) < 510)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 510 && pins.analogReadPin(AnalogPin.P1) < 530 && (pins.analogReadPin(AnalogPin.P2) > 0 && pins.analogReadPin(AnalogPin.P2) < 115)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 510 && pins.analogReadPin(AnalogPin.P1) < 530 && (pins.analogReadPin(AnalogPin.P2) > 530 && pins.analogReadPin(AnalogPin.P2) < 760)) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (pins.analogReadPin(AnalogPin.P1) > 510 && pins.analogReadPin(AnalogPin.P1) < 530 && (pins.analogReadPin(AnalogPin.P2) > 760 && pins.analogReadPin(AnalogPin.P2) < 1024)) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
